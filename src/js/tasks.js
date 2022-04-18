6abb9353113dd6d029301323f26bf30471aba947
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2022-04-18 14:44:47 -0300

    ajustando alterações realizadas pelo Vinicius e correção de pequenos bug's

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -102,7 +102,6 @@ window.addEventListener("load", async () =>{
         let dataTarefa = document.createElement("p");
         let check = document.createElement("input");
         let lixeira = document.createElement("div");
-        
         let iconeLixeira = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -128,8 +127,9 @@ window.addEventListener("load", async () =>{
         check.setAttribute("type", "checkbox")
         caixaCheck.appendChild(check)
         itemListaTarefas.appendChild(caixaCheck);
-        itemListaTarefas.appendChild(lixeira);
         itemListaTarefas.appendChild(caixaTextoData);
+        lixeira.classList.add("delete")
+        itemListaTarefas.appendChild(lixeira);
         
         
 
@@ -151,6 +151,9 @@ window.addEventListener("load", async () =>{
     
 });
 
+
+
+
 /* Adiciona uma nova tarefa */
 
 let formNovaTarefa = document.getElementById("formAdicionaTarefa");
@@ -162,11 +165,7 @@ let botaoTarefa = document.getElementById("botaoTarefa");
 formNovaTarefa.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-<<<<<<< HEAD
     if(inputNovaTarefa.value.length > 0){
-=======
-    if(inputNovaTarefa){
->>>>>>> 51bad1dafe09615649ba93f1b50bc47e96d5b393
         let body = {
             "description": `${inputNovaTarefa.value}`,
             "completed": false
@@ -197,10 +196,6 @@ formNovaTarefa.addEventListener("submit", (evento) => {
     
 })
 
-<<<<<<< HEAD
-/* Atualizando a tarefa como concluida */
-let checkbox = document.getElementsByTagName("input")
-=======
 
 
 
@@ -214,4 +209,3 @@ botaoSair.addEventListener('click', () =>{
     alert("volte sempre!")
 
 })
->>>>>>> 51bad1dafe09615649ba93f1b50bc47e96d5b393
